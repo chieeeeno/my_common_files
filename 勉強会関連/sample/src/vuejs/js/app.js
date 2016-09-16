68cbe2af14c0ed667773b9b8973551d0393131c5
菊地友輝 <Kikuchi.Tomoki@trans-cosmos.co.jp>
2016-09-16 18:44:32 +0900

    vue.jsのサンプル

--- 勉強会関連/sample/src/vuejs/js/app.js
+++ 勉強会関連/sample/src/vuejs/js/app.js
@@ -1,13 +1,34 @@
 ;(function(){
 
   var app = new Vue({
-    el: '#app',
+    el: '#app',     //対象の要素
     data: {
-      message: 'Hello Vue.js!'
+      // message:'',
+      items:[]
     },
     methods: {
       load: function () {
-        alert(this.message);
+        var _this = this;
+        $.ajax({
+          type: 'GET',
+          url: './data/data.json'
+        }).done(function( response ) {
+          _this.items = response
+        });
+      },
+      orderByAsc: function(){
+        this.items.sort(function(a,b){
+          if(a.number<b.number) return -1;
+          if(a.number > b.number) return 1;
+          return 0;
+        });
+      },
+      orderByDesc: function(){
+        this.items.sort(function(a,b){
+          if(a.number>b.number) return -1;
+          if(a.number < b.number) return 1;
+          return 0;
+        });
       }
     }
   })
